5c4f7532f334b7f0b7d25fc5436fb943a6098a7c
oskin1 <ilya.arcadich@gmail.com>
2021-10-01 17:50:57 +0300

    PlutusData WASM interop.

--- src/cardano/entities/plutusData.ts
+++ src/cardano/entities/plutusData.ts
@@ -1,6 +1,8 @@
 import {Hash32} from "../types"
-import {notImplemented} from "../../utils/notImplemented"
 import {stringToBytea} from "../../utils/utf8"
+import * as wasm from "@emurgo/cardano-serialization-lib-browser"
+import {RustModule} from "../../utils/rustLoader"
+import {encodeHex} from "../../utils/hex"
 
 export enum PlutusDataKind {
   Map,
@@ -50,6 +52,29 @@ export function PlutusBytes(value: Uint8Array): PlutusBytes {
   }
 }
 
+export function plutusDataToWasm(data: PlutusData): wasm.PlutusData {
+  switch (data.kind) {
+    case PlutusDataKind.Integer:
+      return RustModule.CardanoWasm.PlutusData.new_integer(
+        RustModule.CardanoWasm.BigInt.from_str(data.value.toString())
+      )
+    case PlutusDataKind.Bytes:
+      return RustModule.CardanoWasm.PlutusData.new_bytes(data.value)
+    case PlutusDataKind.List:
+      const plist = RustModule.CardanoWasm.PlutusList.new()
+      for (const dt of data.value) {
+        plist.add(plutusDataToWasm(dt))
+      }
+      return RustModule.CardanoWasm.PlutusData.new_list(plist)
+    case PlutusDataKind.Map:
+      const pmap = RustModule.CardanoWasm.PlutusMap.new()
+      for (const [k, v] of data.value.entries()) {
+        pmap.insert(plutusDataToWasm(k), plutusDataToWasm(v))
+      }
+      return RustModule.CardanoWasm.PlutusData.new_map(pmap)
+  }
+}
+
 export function hashPlutusData(data: PlutusData): Hash32 {
-  return notImplemented([data])
+  return encodeHex(RustModule.CardanoWasm.hash_plutus_data(plutusDataToWasm(data)).to_bytes())
 }
