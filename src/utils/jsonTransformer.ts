6c983e46e69c2fb89030316399f2c1e2d817354e
rsalakhov <r.salahov@spectrumlabs.fi>
2022-04-08 00:53:25 +0300

    fix jsonTransformer types

--- src/utils/jsonTransformer.ts
+++ src/utils/jsonTransformer.ts
@@ -1,15 +1,14 @@
 type Unpacked<T> = T extends (infer U)[]
   ? U
   : T extends (...args: any[]) => infer U
-    ? U
-    : T extends Promise<infer U>
-    ? U
-    : T;
+  ? U
+  : T extends Promise<infer U>
+  ? U
+  : T;
 
 type TransformerFn<T> = (v: any) => T;
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type TransformerObj<T> = T extends object
+type TransformerObj<T> = T extends Record<string, any>
   ? { [key in keyof T]: TransformerObj<T[key]> }
   : TransformerFn<T>;
 
@@ -25,13 +24,13 @@ export class JsonTransformer<T> {
 
   private transformKey(
     value: any,
-    rules: Partial<TransformerObj<Unpacked<Unpacked<any>>>> | undefined
+    rules: Partial<TransformerObj<Unpacked<any>>> | undefined
   ): any {
     if (value instanceof Array) {
       return value.map((cv) => this.transformKey(cv, rules))
     }
     if (value instanceof Object) {
-      return Object.entries(value).reduce<any>((acc, [key, cv]) => {
+      return Object.entries(value).reduce<Record<string, any>>((acc, [key, cv]) => {
         acc[key] = this.transformKey(
           cv,
           rules ? (rules as any)[key] : undefined
@@ -41,6 +40,6 @@ export class JsonTransformer<T> {
       }, {})
     }
 
-    return rules ? (rules as any)(value) : value
+    return rules && rules instanceof Function ? rules(value) : value
   }
 }
