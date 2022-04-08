cbb9448cf3e3ba7e23b5976228b86a8706cdc9f1
oskin1 <ilya.arcadich@gmail.com>
2022-04-08 16:01:27 +0300

    Complete pools.

--- src/utils/jsonTransformer.ts
+++ src/utils/jsonTransformer.ts
@@ -4,17 +4,16 @@ type Unpacked<T> = T extends (infer U)[]
   ? U
   : T extends Promise<infer U>
   ? U
-  : T;
+  : T
 
-type TransformerFn<T> = (v: any) => T;
+type TransformerFn<T> = (v: any) => T
 
 type TransformerObj<T> = T extends Record<string, any>
-  ? { [key in keyof T]: TransformerObj<T[key]> }
-  : TransformerFn<T>;
+  ? {[key in keyof T]: TransformerObj<T[key]>}
+  : TransformerFn<T>
 
 export class JsonTransformer<T> {
-  constructor(private rules: Partial<TransformerObj<Unpacked<Unpacked<T>>>>) {
-  }
+  constructor(private rules: Partial<TransformerObj<Unpacked<Unpacked<T>>>>) {}
 
   transform(raw: string): T {
     const parsed = JSON.parse(raw)
@@ -22,19 +21,13 @@ export class JsonTransformer<T> {
     return this.transformKey(parsed, this.rules)
   }
 
-  private transformKey(
-    value: any,
-    rules: Partial<TransformerObj<Unpacked<any>>> | undefined
-  ): any {
+  private transformKey(value: any, rules: Partial<TransformerObj<Unpacked<any>>> | undefined): any {
     if (value instanceof Array) {
-      return value.map((cv) => this.transformKey(cv, rules))
+      return value.map(cv => this.transformKey(cv, rules))
     }
     if (value instanceof Object) {
       return Object.entries(value).reduce<Record<string, any>>((acc, [key, cv]) => {
-        acc[key] = this.transformKey(
-          cv,
-          rules ? (rules as any)[key] : undefined
-        )
+        acc[key] = this.transformKey(cv, rules ? (rules as any)[key] : undefined)
 
         return acc
       }, {})
