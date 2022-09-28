3d9aecc01da15985ad8fcc12d2180c76137a88d8
rsalakhov <r.salahov@spectrumlabs.fi>
2022-09-29 00:21:29 +0300

    fix pool script

--- src/amm/scripts.ts
+++ src/amm/scripts.ts
@@ -20,7 +20,7 @@ export type ScriptCreds = {
 }
 
 export const ScriptCredsV1: ScriptCreds = {
-  ammPool: "b9eebc6add3b4684debb0d91c5da5677ae308279786c1e955ee34942",
+  ammPool: "a258f896dff1d01ac9a8bd0598304b933a8f3e9e0953938767178099",
   ammDeposit: "075e09eb0fa89e1dc34691b3c56a7f437e60ac5ea67b338f2e176e20",
   ammRedeem: "83da79f531c19f9ce4d85359f56968a742cf05cc25ed3ca48c302dee",
   ammSwap: "18f16aa47943ee2aea9e9deddbad458a02c656480fae5c3e2343ce92"
