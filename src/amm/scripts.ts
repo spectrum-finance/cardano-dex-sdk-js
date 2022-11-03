e0d787a2cc1e926e875c61c94bdd4ed7ac9d6fb9
rsalakhov <r.salahov@spectrumlabs.fi>
2022-11-03 18:37:10 +0300

    update scripts and version 2

--- src/amm/scripts.ts
+++ src/amm/scripts.ts
@@ -9,7 +9,7 @@ export type OrderAddrs = {
 export const OrderAddrsV1Testnet: OrderAddrs = {
   ammDeposit: "addr_test1wqr4uz0tp75fu8wrg6gm83t20aphuc9vt6n8kvu09ctkugq6ch8kj",
   ammRedeem: "addr_test1wzpa5704x8qel88ympf4natfdzn59nc9esj7609y3sczmmsxcd9lz",
-  ammSwap: "addr_test1wzqdtrymkxze8kkfh4jv4ysh9wjrlwq0swl5vkcgyxtm7wcv676ry"
+  ammSwap: "addr_test1wqnp362vmvr8jtc946d3a3utqgclfdl5y9d3kn849e359hst7hkqk"
 }
 
 export type ScriptCreds = {
@@ -23,6 +23,6 @@ export const ScriptCredsV1: ScriptCreds = {
   ammPool: "a258f896dff1d01ac9a8bd0598304b933a8f3e9e0953938767178099",
   ammDeposit: "075e09eb0fa89e1dc34691b3c56a7f437e60ac5ea67b338f2e176e20",
   ammRedeem: "83da79f531c19f9ce4d85359f56968a742cf05cc25ed3ca48c302dee",
-  ammSwap: "80d58c9bb18593dac9bd64ca92172ba43fb80f83bf465b082197bf3b"
+  ammSwap: "2618e94cdb06792f05ae9b1ec78b0231f4b7f4215b1b4cf52e6342de"
 }
 
