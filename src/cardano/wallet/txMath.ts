8bfffc20fb4107beaa65cc9e94ddb00298b2867e
rsalakhov <r.salahov@spectrumlabs.fi>
2023-06-13 19:50:12 +0300

    add log to tx math

--- src/cardano/wallet/txMath.ts
+++ src/cardano/wallet/txMath.ts
@@ -56,6 +56,7 @@ class CardanoTransactionMath implements TxMath {
       const pd = this.R.PlutusData.from_bytes(decodeHex(o.data))
       out.set_plutus_data(pd)
     }
+    console.log(out);
     return out
   }
 }
