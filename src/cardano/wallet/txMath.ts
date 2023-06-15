4f946cf4d3cb64cadbca5f0831abf97ba1a7be71
rsalakhov <r.salahov@spectrumlabs.fi>
2023-06-15 16:23:08 +0300

    update txMath module

--- src/cardano/wallet/txMath.ts
+++ src/cardano/wallet/txMath.ts
@@ -7,6 +7,8 @@ import {ProtocolParams} from "../entities/env"
 import {TxOutCandidate} from "../entities/txOut"
 import {Value} from "../entities/value"
 
+const INSUFFICIENT_BYTES = 2;
+
 export interface TxMath {
   minUtxoValue(preValue: Value, hasDatum: boolean): bigint
 
@@ -25,14 +27,17 @@ class CardanoTransactionMath implements TxMath {
   minUtxoValue(preValue: Value, hasDatum: boolean): bigint {
     const value = toWasmValue(preValue, this.R)
     const utxoCost = this.R.BigNum.from_str(this.params.utxoCostPerWord.toString())
-    return BigInt(this.R.min_ada_required(value, hasDatum, utxoCost).to_str())
+    return BigInt(this.R.min_ada_required(value, hasDatum, utxoCost).to_str()) +
   }
 
   minAdaRequiredforOutput(outCandidate: TxOutCandidate): bigint {
     const out = this.toTransactionOutput(outCandidate);
     const dataCost = this.R.DataCost.new_coins_per_byte(this.R.BigNum.from_str(this.params.coinsPerUtxoByte.toString()));
 
-    return BigInt(this.R.min_ada_for_output(out, dataCost).to_str());
+    /** TODO: Investigate insufficient byte for min utxo calculation issue. Compare utxo sizes between cardano bots and frontend sdk.
+     * Looks like cardano ser lib issue
+    */
+    return BigInt(this.R.min_ada_for_output(out, dataCost).to_str()) + BigInt(Number(this.params.coinsPerUtxoByte) * INSUFFICIENT_BYTES);
   }
 
   maxByAssetAndAdaBudget(assetBudget: AssetAmount, adaBudget: bigint, addr: string): AssetAmount {
