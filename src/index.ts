c7746af4c383392a34abb5f21b7aa0717b629772
rsalakhov <r.salahov@spectrumlabs.fi>
2023-06-23 09:39:01 +0300

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -96,7 +96,7 @@ export {PoolsParser, mkPoolsParser} from "./amm/parsers/poolsParser"
 export {Pools, mkNetworkPoolsV1} from "./amm/services/pools"
 export {History, mkHistory} from "./amm/services/history"
 export * from "./amm/constants"
-export {ScriptCreds, OrderAddrs, ScriptCredsV1, OrderAddrsV1Testnet, OrderAddrsV1Mainnet} from "./amm/scripts"
+export {ScriptCreds, OrderAddrs, ScriptCredsV1, OrderAddrsV1Testnet, OrderAddrsV1Mainnet, OpInRefsMainnetV1, OpInRefsPreviewV1} from "./amm/scripts"
 export * from "./amm/contractData"
 
 export {
