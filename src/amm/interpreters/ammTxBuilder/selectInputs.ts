1ba759f14629750abbb07b3616bf2cd46e07f03e
rsalakhov <r.salahov@spectrumlabs.fi>
2023-07-23 17:02:03 +0300

    update 0.1.122 (update change algo calculations)

---
+++ src/amm/interpreters/ammTxBuilder/selectInputs.ts
@@ -0,0 +1,39 @@
+import {AdaEntry} from "../../../cardano/entities/assetEntry"
+import {FullTxIn} from "../../../cardano/entities/txIn"
+import {remove, sum, Value} from "../../../cardano/entities/value"
+import {InputSelector} from "../../../cardano/wallet/inputSelector"
+import {TxMath} from "../../../cardano/wallet/txMath"
+import {getChangeOrderValue} from "../../../utils/getChangeOrderValue"
+
+
+export const selectInputs = async (
+  totalOrderBudget: Value,
+  changeAddress: string,
+  inputSelector: InputSelector,
+  txMath: TxMath): Promise<FullTxIn[] | Error> => {
+  const inputs = await inputSelector.select(totalOrderBudget)
+
+  if (inputs instanceof Error) {
+    throw new Error("insufficient funds")
+  }
+
+  const normalizeChange = async (inputs: FullTxIn[]): Promise<FullTxIn[] | Error> => {
+    const estimatedChange = remove(
+      sum(inputs.map(input => input.txOut.value)),
+      totalOrderBudget
+    );
+    const [, additionalAdaForChange] = getChangeOrderValue(estimatedChange, changeAddress, txMath);
+    if (additionalAdaForChange) {
+      const additionalInput = await inputSelector.select([AdaEntry(additionalAdaForChange)], inputs);
+
+      if (additionalInput instanceof Error) {
+        throw new Error("insufficient funds")
+      }
+      return normalizeChange(inputs.concat(additionalInput));
+    } else {
+      return Promise.resolve(inputs);
+    }
+  }
+
+  return normalizeChange(inputs);
+}
