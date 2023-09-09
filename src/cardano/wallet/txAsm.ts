357d279debb580e487c86d50e718f9d16c574dc9
rsalakhov <r.salahov@spectrumlabs.fi>
2023-09-09 15:18:21 +0300

    update plutus v

--- src/cardano/wallet/txAsm.ts
+++ src/cardano/wallet/txAsm.ts
@@ -167,7 +167,7 @@ class DefaultTxAsm implements TxAsm {
     const mintBuilder = this.R.MintBuilder.new();
 
     for (const [i, data] of mintScripts.entries()) {
-      const plutusScriptSource = this.R.PlutusScriptSource.new(this.R.PlutusScript.new_v2(decodeHex(data.script)));
+      const plutusScriptSource = this.R.PlutusScriptSource.new(this.R.PlutusScript.new_with_version(decodeHex(data.script), this.R.Language.new_plutus_v2()));
       const redeemer = this.R.Redeemer.new(
         this.R.RedeemerTag.new_mint(),
         this.R.BigNum.from_str((mintScripts.length - 1 - i).toString()),
