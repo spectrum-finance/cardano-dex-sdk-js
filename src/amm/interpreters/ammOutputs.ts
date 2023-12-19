0e95ca6d50ed2bf14406293a9a957691292a5292
rsalakhov <r.salahov@spectrumlabs.fi>
2023-12-19 18:37:35 +0300

    try to add bidirectional fee #5

--- src/amm/interpreters/ammOutputs.ts
+++ src/amm/interpreters/ammOutputs.ts
@@ -62,7 +62,7 @@ class AmmOutputsImpl implements AmmOutputs {
     const userOutputValue = Value(req.minAdaForUserOutput, calculateInitUserRewardLq(req.x, req.y, req.lq.asset))
     const poolOutput = {
       value: remove(add(add(req.poolValue, req.lq.toEntry), req.nft.toEntry), userOutputValue),
-      addr: 'addr1x94ec3t25egvhqy2n265xfhq882jxhkknurfe9ny4rl9k6dj764lvrxdayh2ux30fl0ktuh27csgmpevdu89jlxppvrst84slu',
+      addr: 'addr1w9h9hxmlhatfppukqmh4vfq992dqpnumx4qaqkw8cc25vxcfn9439',
       data
     }
     const userLqOutput = {
