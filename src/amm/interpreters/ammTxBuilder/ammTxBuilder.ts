797a25822425cea49e70388d820624558d601f1d
rsalakhov <r.salahov@spectrumlabs.fi>
2024-02-02 14:30:23 +0300

    update fee coef

--- src/amm/interpreters/ammTxBuilder/ammTxBuilder.ts
+++ src/amm/interpreters/ammTxBuilder/ammTxBuilder.ts
@@ -284,11 +284,11 @@ export class DefaultAmmTxCandidateBuilder implements AmmTxBuilder {
 
     try {
       const transaction = this.txAsm.finalize(unlockTxCandidate)
-      const txFee =  BigInt(Math.floor(Number(transaction.body().fee().to_str())))
+      const txFee =  BigInt(Math.floor(Number(transaction.body().fee().to_str()) * 1.5))
 
       if (prevTxFee === txFee) {
         return [
-          this.txAsm.finalize(unlockTxCandidate),
+          this.txAsm.finalize(unlockTxCandidate, 1.5),
           unlockTxCandidate,
           unlockTxInfo,
           null
